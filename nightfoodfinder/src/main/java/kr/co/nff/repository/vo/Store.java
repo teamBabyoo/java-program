b744781d755d1862bc5ebebd3e9ce1a6b98a6003
joy <sngbrm99@gmail.com>
2019-11-13 17:26:57 +0900

    store vo 수정

--- nightfoodfinder/src/main/java/kr/co/nff/repository/vo/Store.java
+++ nightfoodfinder/src/main/java/kr/co/nff/repository/vo/Store.java
@@ -1,5 +1,22 @@
 package kr.co.nff.repository.vo;
 
-public class Store {
+import lombok.Data;
 
+@Data
+public class Store {
+	private int storeNo;
+	private String storeName;
+	private String storeEmail;
+	private String storePass;
+	private int storeTell;
+	private int storeCategory;
+	private String openTime;
+	private String closeTime;
+	private int areaCode;
+	private int status;
+	private int businessNum;
+	private String storeOwner;
+	private int storeOwnerPh;
+//	private String latitude;
+//	private String longitude;
 }
