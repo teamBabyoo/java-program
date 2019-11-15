89ed06b19576b791ade7308f0fed56b573b94922
임승경 <sglim9400@gmail.com>
2019-11-15 19:19:50 +0900

    전체 리뷰 리스트 검색

--- nightfoodfinder/src/main/java/kr/co/nff/admin/review/controller/AdminReviewController.java
+++ nightfoodfinder/src/main/java/kr/co/nff/admin/review/controller/AdminReviewController.java
@@ -7,23 +7,24 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 import kr.co.nff.admin.review.service.AdminReviewService;
-
+import kr.co.nff.repository.vo.Search;
 
 @Controller("kr.co.nff.admin.review.controller.AdminReviewController")
 @RequestMapping("/admin/review")
 public class AdminReviewController {
-	
+
 	@Autowired
 	private AdminReviewService service;
 
-	//리뷰 목록
+	// 리뷰 목록
 	@RequestMapping("/reviewlist.do")
-	public void reviewList(@RequestParam(value="pageNo", defaultValue="1") int pageNo, Model model) {
-		model.addAttribute("list", service.listReview());
+	public void reviewList(@RequestParam(required = false, defaultValue = "nickName") String searchType,
+			@RequestParam(required = false) String keyword, Model model) {
+
+		Search search = new Search();
+		search.setType(searchType);
+		search.setKeyword(keyword);
+		model.addAttribute("list", service.listReview(search));
 	}
-	
 
-	
-	
 }
-
--- nightfoodfinder/src/main/java/kr/co/nff/repository/dao/ReviewDAO.java
+++ nightfoodfinder/src/main/java/kr/co/nff/repository/dao/ReviewDAO.java
@@ -3,7 +3,8 @@ package kr.co.nff.repository.dao;
 import java.util.List;
 
 import kr.co.nff.repository.vo.Review;
+import kr.co.nff.repository.vo.Search;
 
 public interface ReviewDAO {
-	List<Review> selectReview();
+	List<Review> selectReview(Search search);
 }
