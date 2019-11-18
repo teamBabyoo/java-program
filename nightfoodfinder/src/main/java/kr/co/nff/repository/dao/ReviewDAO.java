eea8bdd2adc2ae81e8887b806691ec688c539445
임승경 <sglim9400@gmail.com>
2019-11-18 20:57:41 +0900

    전체 리뷰 페이징

--- nightfoodfinder/src/main/java/kr/co/nff/repository/dao/ReviewDAO.java
+++ nightfoodfinder/src/main/java/kr/co/nff/repository/dao/ReviewDAO.java
@@ -2,9 +2,13 @@ package kr.co.nff.repository.dao;
 
 import java.util.List;
 
+
 import kr.co.nff.repository.vo.Review;
-import kr.co.nff.repository.vo.Search;
+import kr.co.nff.repository.vo.SearchRe;
+
+
 
 public interface ReviewDAO {
-	List<Review> selectReview(Search search);
+	List<Review> selectReview(SearchRe search);
+	int pagingGetListCnt(SearchRe search);
 }
