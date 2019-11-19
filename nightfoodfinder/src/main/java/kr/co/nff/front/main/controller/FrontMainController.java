fb4a3d24faa6884c251554b464f74f8007832250
유지인 <esther.yoo329@gmail.com>
2019-11-19 09:53:42 +0900

    메인페이지 5개 가게리스트 가져오기

--- nightfoodfinder/src/main/java/kr/co/nff/front/main/controller/FrontMainController.java
+++ nightfoodfinder/src/main/java/kr/co/nff/front/main/controller/FrontMainController.java
@@ -2,29 +2,26 @@ package kr.co.nff.front.main.controller;
 
 import java.util.List;
 
-import javax.servlet.http.HttpSession;
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
-import kr.co.nff.front.main.service.Service;
-import kr.co.nff.repository.vo.Category;
+import kr.co.nff.front.main.service.MainService;
+import kr.co.nff.repository.vo.Store;
 
 @Controller
 @RequestMapping("/front/main")
 public class FrontMainController {
 	@Autowired
-	private Service service;
+	private MainService service;
 	
-	
+	// 최상위 5개 스토어
 	@RequestMapping("/main.do")
-	public void main(HttpSession session) {
-		List<Category> cateList = service.selectCategory();
-		System.out.println(cateList);
-		session.setAttribute("cateList", cateList);
-		
-//		System.out.println("와써");
-//		System.out.println(service.selectUser());
-	}
+	public void main(Model model) {
+//		System.out.println("컨트롤러 OK");
+		model.addAttribute("sList", service.storeList());
+//		List<Store> store = service.storeList();
+//		System.out.println("컨트롤러 : " + service.storeList());
+	} 
 }
--- nightfoodfinder/src/main/webapp/index.jsp
+++ nightfoodfinder/src/main/webapp/index.jsp
@@ -12,4 +12,5 @@
 		location.href="${pageContext.request.contextPath}/front/main/main.do";
 	</script>
 </body>
+
 </html>
