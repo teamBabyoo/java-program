76862fbcd6fac20b3df12c29230f86aa0fa976c4
유지인 <esther.yoo329@gmail.com>
2019-11-28 03:45:26 +0900

    리뷰 댓글 등록 작업중
    .

--- nightfoodfinder/src/main/java/kr/co/nff/front/store/controller/FrontStoreController.java
+++ nightfoodfinder/src/main/java/kr/co/nff/front/store/controller/FrontStoreController.java
@@ -1,5 +1,7 @@
 package kr.co.nff.front.store.controller;
 
+import java.io.File;
+import java.io.IOException;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,6 +10,8 @@ import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.multipart.MultipartFile;
+import org.springframework.web.multipart.MultipartHttpServletRequest;
 
 import kr.co.nff.front.store.service.StoreService;
 import kr.co.nff.repository.vo.Review;
@@ -73,7 +77,46 @@ public class FrontStoreController {
 		return service.reviewList(no);
 	}
 
-	
+	/* 리뷰 작성 & 이미지 업로드 */
+	@RequestMapping("/review_regist.do")
+	@ResponseBody
+//	public List<Review> reviewRegistAjax(Review review, MultipartHttpServletRequest mtfRequest) {
+	public List<Review> reviewRegistAjax(Review review) {
+		System.out.println("리뷰등록 시도");
+/*		
+		List<MultipartFile> fileList = mtfRequest.getFiles("file");
+        String src = mtfRequest.getParameter("src");
+        System.out.println("src value : " + src);
+
+        String path = "C:\\image\\";
+
+        for (MultipartFile mf : fileList) {
+            String originFileName = mf.getOriginalFilename(); // 원본 파일 명
+            long fileSize = mf.getSize(); // 파일 사이즈
+
+            System.out.println("originFileName : " + originFileName);
+            System.out.println("fileSize : " + fileSize);
+
+            String safeFile = path + System.currentTimeMillis() + originFileName;
+            try {
+                mf.transferTo(new File(safeFile));
+            } catch (IllegalStateException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            } catch (IOException e) {
+                // TODO Auto-generated catch block
+                e.printStackTrace();
+            }
+        }
+*/
+//        model.addAttribute("list", service.reviewRegist(review));
+		System.out.println("내용 : " + review.getReviewContent());
+		System.out.println("답댓 : " + review.getRecomment());
+		System.out.println("스코프 : " + review.getStoreScope());
+        System.out.println("게시글번호 확인" + review.getStoreNo());
+        service.reviewRegist(review);
+        return service.reviewList(review.getStoreNo());
+	}
 
 	
 }
