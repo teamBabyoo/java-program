8a292bf238064f562b30435be387e2c5f93e0370
agheha <youbeen0712@gmail.com>
2019-12-04 21:03:01 +0900

    알림 쿼리 및 서비스 작업

---
+++ nightfoodfinder/src/main/java/kr/co/nff/repository/dao/NoticeDAO.java
@@ -0,0 +1,19 @@
+package kr.co.nff.repository.dao;
+
+import java.util.List;
+
+import kr.co.nff.repository.vo.Notice;
+
+public interface NoticeDAO {
+	// 알림 생성
+	void insertNotice(Notice notice);
+	
+	// 알림 리스트
+	List<Notice> selectNotice(Notice notice);
+	
+	// 알림 읽음 처리
+	void readNotice(Notice notice);
+	
+	// 새알림 갯수
+	int countNewNotice(Notice notice);
+}
