6791a07686283cac4fff4b75550ee98c2d7dc6f8
headeun <headeun93@naver.com>
2019-12-10 20:36:11 +0900

    통계 쿼리 완료

--- nightfoodfinder/src/main/java/kr/co/nff/admin/stat/controller/AdminStatController.java
+++ nightfoodfinder/src/main/java/kr/co/nff/admin/stat/controller/AdminStatController.java
@@ -7,19 +7,21 @@ import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import kr.co.nff.admin.stat.service.StatService;
+import kr.co.nff.repository.vo.Search;
 
 
 @Controller("kr.co.nff.admin.stat.controller.AdminStatController")
 @RequestMapping("/admin/stat")
 public class AdminStatController {
 	
-	@RequestMapping("/statlist.do")
-	public void storeList() {
-		
-	}
-	
 	@Autowired
 	private StatService service;
+	
+	@RequestMapping("/statlist.do")
+	public void storeList(Search search) {
+		service.statFrequentStore(search);
+	}
+	
 	
 	
 	
--- nightfoodfinder/src/main/java/kr/co/nff/admin/stat/service/StatService.java
+++ nightfoodfinder/src/main/java/kr/co/nff/admin/stat/service/StatService.java
@@ -1,7 +1,8 @@
 package kr.co.nff.admin.stat.service;
 
-
+import kr.co.nff.repository.vo.Search;
+import kr.co.nff.repository.vo.Stat;
 
 public interface StatService {
-
+	Stat statFrequentStore(Search search);
 }
