a5b7db5821f705f92233eb4e80784e4b76856128
유지인 <esther.yoo329@gmail.com>
2019-12-22 18:43:15 +0900

    리뷰
    리뷰 등록시 가게번호 파일테이블 저장
    리뷰 삭제시 파일테이블 업데이트

--- nightfoodfinder/src/main/java/kr/co/nff/util/FileUpload.java
+++ nightfoodfinder/src/main/java/kr/co/nff/util/FileUpload.java
@@ -1,13 +1,9 @@
 package kr.co.nff.util;
 
-import java.util.List;
-
-import org.springframework.web.multipart.MultipartFile;
-
-import kr.co.nff.repository.vo.FileVO;
+import kr.co.nff.repository.vo.Review;
 
 public interface FileUpload {
 	// 이미지 업로드하는 리뷰등록
-	public int upload(List<MultipartFile> attach) throws Exception;
+	public int upload(Review review) throws Exception;
 	
 }
