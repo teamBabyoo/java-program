2f383b055be52b0c84384e87d9d8d3945cdfe14e
trueekim <kim_truee@naver.com>
2019-12-28 15:49:54 +0900

    true/front-storedetail;
    list.js에 내 위치 받을 코드 추가,
    주소 localhost로 변경

--- nightfoodfinder/src/main/webapp/resources/js/storelist.js
+++ nightfoodfinder/src/main/webapp/resources/js/storelist.js
@@ -364,6 +364,23 @@ jQuery.ajax({
 });
 }
 
+/* 내 위치값 받기*/
+
+$(function() { 
+    // Geolocation API에 액세스할 수 있는지를 확인
+    if (navigator.geolocation) {
+        //위치 정보를 얻기
+        navigator.geolocation.getCurrentPosition (function(pos) {
+            y = (pos.coords.latitude);// 위도
+            x = (pos.coords.longitude);// 경도
+            console.log("내위치: ", y, x);
+        });
+    } else {
+        alert("이 브라우저에서는 Geolocation이 지원되지 않습니다.")
+    }
+
+}); /*   
+*/
 
 
 
